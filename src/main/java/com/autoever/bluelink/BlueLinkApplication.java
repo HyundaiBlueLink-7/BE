9fe457c4ab7b9c2ed6f6a0453f28fd29f6245ebe
zoonmy <pjm2571@naver.com>
2024-12-04 09:40:17 +0900

    init: 초기 프로젝트 설정

---
+++ src/main/java/com/autoever/bluelink/BlueLinkApplication.java
@@ -0,0 +1,13 @@
+package com.autoever.bluelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlueLinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/autoever/bluelink/BlueLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoever.bluelink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
